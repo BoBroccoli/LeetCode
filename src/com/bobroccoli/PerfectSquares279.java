418a6ebadaaa8d69f0c0ba80f8f84fd977a27bfe
BoBroccoli <wenxx194@d.umn.edu>
2019-06-10 12:53:05 -0500

    Commit

--- src/com/bobroccoli/PerfectSquares279.java
+++ src/com/bobroccoli/PerfectSquares279.java
@@ -1,5 +1,20 @@
 package com.bobroccoli;
 
 public class PerfectSquares279 {
-
+    public int numSquares(int n) {
+    	if(n <= 0)
+    		return 0;
+    	int[] dp = new int[n+1];
+    	dp[0] = 0;
+    	dp[1] = 1;
+    	for(int i = 0; i < n+1; i++) {
+    		dp[i] = dp[i-1] + 1;
+    		int j = 1;
+    		while(j * j <= i) {
+    			dp[i] = Math.min(dp[i - j*j] + 1, dp[i]);
+    			j++;
+    		}
+    	}
+    	return dp[n];
+    }
 }
--- src/com/bobroccoli/SubArraySumEqualsK560.java
+++ src/com/bobroccoli/SubArraySumEqualsK560.java
@@ -7,9 +7,10 @@ public class SubArraySumEqualsK560 {
     public int subarraySum(int[] nums, int k) {
     	int res = 0, sum = 0;
     	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    	map.put(0, 1);
     	for(int i = 0; i < nums.length; i++) {
     		sum += nums[i];
-    		if(map.containsKey(sum))
+    		if(map.containsKey(sum-k))
     			res += map.get(sum-k);
     		map.put(sum, map.getOrDefault(sum, 0)+1);
     	}
