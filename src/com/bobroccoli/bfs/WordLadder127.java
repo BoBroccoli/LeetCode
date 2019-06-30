a592036dcc88d2055d9f3d138f5744225e503e92
Bo <wenxx194@d.umn.edu>
2019-06-30 15:43:31 -0500

    bfs

--- src/com/bobroccoli/bfs/WordLadder127.java
+++ src/com/bobroccoli/bfs/WordLadder127.java
@@ -1,8 +1,10 @@
 package com.bobroccoli.bfs;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 
 public class WordLadder127 {
 	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
@@ -36,7 +38,45 @@ public class WordLadder127 {
 			return 0;
 		return res;
 	}
-
+	//double ended BST
+	public int ladderLengthTwo(String beginWord, String endWord, List<String> wordList) {
+		int res = 0;
+		if (wordList == null || wordList.size() == 0)
+			return res;
+		Set<String> startq = new HashSet<String>();
+		Set<String> endq = new HashSet<String>();
+		Set<String> visited = new HashSet<>();
+		startq.add(beginWord);
+		visited.add(beginWord);
+		res++;
+		endq.add(endWord);
+		visited.add(endWord);
+		while (!startq.isEmpty() && !endq.isEmpty()) {
+			Set<String> curSet = startq;
+			Set<String> targetSet = endq;
+			if(res % 2 == 0) {
+				curSet = endq;
+				targetSet = startq;
+			}
+			Set<String> temp = new HashSet<>();
+			for(String string : curSet) {
+				if(targetSet.contains(string))
+					return res;
+				for(int i = 0; i < wordList.size(); i++) {
+					if(!visited.contains(wordList.get(i)) && oneDistance(wordList.get(i), string)) {
+						if(res % 2 ==0) {
+							endq = temp;
+							visited.add(wordList.get(i));
+						}
+						else
+							startq = temp;
+					}
+				}
+			}
+			++res;
+		}
+		return 0;
+	}
 	public boolean oneDistance(String a, String b) {
 		if (a.length() != b.length())
 			return false;
