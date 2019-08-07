98d68982c1a6f0ce52cee074b7d4903e8fb2abe6
BoBroccoli <wenxx194@d.umn.edu>
2019-08-06 21:24:35 -0400

    Binary sort

---
+++ src/com/bobroccoli/binarysearch/SingleElementinaSortedArray540.java
@@ -0,0 +1,26 @@
+package com.bobroccoli.binarysearch;
+
+public class SingleElementinaSortedArray540 {
+	public int singleNonDuplicate(int[] nums) {
+		//The length of array must be odd
+		int left = 0, right = nums.length-1;
+		while(left < right){
+			int mid = left + (right-left)/2;
+			if(mid % 2 == 0) {
+				//even position
+				if(nums[mid] == nums[mid+1])
+					left = mid+2;
+				else
+					right = mid;
+			}
+			else {
+				//odd position
+				if(nums[mid] != nums[mid+1])
+					left = mid+1;
+				else
+					right = mid-1;
+			}
+		}
+		return nums[left];
+	}
+}
